9cb9f5f8ab1fc691e9d73df67677f8672f9513e1
Sunshow <sunshow@gmail.com>
2021-11-30 14:13:25 +0800

    add publishing settings

---
+++ foundation-base/src/main/java/net/sunshow/fundation/base/enumeration/IBaseEnum.java
@@ -0,0 +1,19 @@
+package net.sunshow.fundation.base.enumeration;
+
+/**
+ * 枚举基础接口
+ * @author sunshow
+ */
+public interface IBaseEnum {
+    /**
+     * 获取枚举值
+     * @return 枚举值
+     */
+    int getValue();
+
+    /**
+     * 获取枚举名称
+     * @return 枚举名称
+     */
+    String getName();
+}
